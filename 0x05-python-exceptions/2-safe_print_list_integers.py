8792fe3c4bf08926e5c08e923dc7b6a60323fd1d
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-26 22:21:57 -0700

    debug mode

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0 # Counter for the number of integers printed
+
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+    except IndexError:
+        pass
+
+    print() # Print a new line after all integers have been printed
+    return count
