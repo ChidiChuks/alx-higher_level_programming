a1e89d1626243bd029d667bc2002d2a0b0050276
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-26 22:28:07 -0700

    corrcted the print format value of data

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        print("{:id}".format(value))
+        print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
         return False
